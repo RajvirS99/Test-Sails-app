cc3f799d9d9e66205a37d840d89b218441ef57cc
Rajvir Singh <rsaggu99@gmail.com>
2020-11-29 21:43:18 +0530

    linting changes

--- api/controllers/DataController.js
+++ api/controllers/DataController.js
@@ -6,33 +6,33 @@
  */
 
 module.exports = {
-    add:function(req, res) {
+    add: function (req, res) {
         let name = req.body.CustName;
         let phone = req.body.PhoneNumber;
         let message = req.body.CustMessage;
 
-        Data.create({Customer_Name: name, Phone_Number: phone, Customer_Message: message}).exec(function(err){
-            if(err){
-                res.send(500, {err: 'Database Error'});
+        Data.create({ Customer_Name: name, Phone_Number: phone, Customer_Message: message }).exec(function (err) {
+            if (err) {
+                res.send(500, { err: 'Database Error' });
             }
 
             res.redirect('/');
         })
     },
 
-    list:function(req,res) {
-        Data.find({}).exec(function(err, data){
-            if(err){
+    list: function (req, res) {
+        Data.find({}).exec(function (err, data) {
+            if (err) {
                 res.send(500, 'Database Error')
             }
-            res.view('read', {read:data});
+            res.view('read', { read: data });
         })
     },
 
-    delete:function(req,res){
-        Data.destroy({ id:req.params.id}).exec(function(err){
-            if(err){
-                res.send(500, {error: 'Database error'})
+    delete: function (req, res) {
+        Data.destroy({ id: req.params.id }).exec(function (err) {
+            if (err) {
+                res.send(500, { error: 'Database error' })
             }
             res.redirect('/read');
         })
